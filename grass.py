7344c6692a68625018057d593ee3b2f587cedcc1
cake <2429239979@qq.com>
2024-04-14 17:03:05 +0800

    新文件：   main.py

---
+++ grass.py
@@ -0,0 +1,154 @@
+import requests
+import pyaes
+from base64 import b64encode
+
+def password_encode(plain_text):
+    key = b'pigxpigxpigxpigx'  # 密钥
+    iv = b'pigxpigxpigxpigx'  # 初始化向量
+    plain_text = plain_text.encode()  # 将字符串转换为字节串
+
+    # 计算 padding 长度
+    pad_len = 16 - (len(plain_text) % 16)
+
+    # 添加 padding
+    plain_text += b'\x00' * pad_len
+
+    # 创建一个 AES CBC cipher
+    cipher = pyaes.AESModeOfOperationCBC(key, iv)
+
+    # 加密
+    cipher_text = b"".join([cipher.encrypt(plain_text[i:i+16]) for i in range(0, len(plain_text), 16)])
+
+    return b64encode(cipher_text).decode('utf-8')  # 将字节串转换为字符串
+
+def get_access_token(username, password):
+    global access_token, studentId
+    password = password_encode(password)
+    data = {'username': username,'password': password}
+    headers = {'Authorization': 'Basic c3R1ZGVudDpzdHVkZW50', 'Content-Type': 'application/x-www-form-urlencoded'}
+    response = requests.post('https://www.wtjy.com/auth/oauth/token?grant_type=password', headers=headers, data=data)
+    response.raise_for_status()  # 检查请求是否成功
+    json_response = response.json()
+    studentId = json_response['user_info']['studentId']
+    access_token = json_response['access_token']
+    return {'access_token': access_token, 'studentId': studentId}
+
+def get_answer_paper(access_token, id, studentId, subjectId):
+    url = "https://www.wtjy.com/report/statappstudentreport/getanswerpaper/v2"
+    params = {'id': id, 'studentId': studentId, 'subjectId': subjectId}
+    headers = {'Authorization': f'Bearer {access_token}', 'Content-Type': 'application/x-www-form-urlencoded'}
+    response = requests.get(url, headers=headers, params=params)
+    response.raise_for_status()  # 检查请求是否成功
+    json_response = response.json()
+    return json_response['data']['answerSheetUrl']
+    
+def fetch_exams_info(access_token):
+    try:
+        url = 'https://www.wtjy.com/report/stat/reportpage'
+        headers = {
+        	'Authorization': f'Bearer {access_token}',
+        	'SCHOOL-ID': '20125',
+        	'Content-Type': 'application/json'
+        	}
+        data = {
+        	"current": 1,
+        	"size": 5
+        	}
+        response = requests.post(url, json=data, headers=headers)
+        response.raise_for_status()
+        exams = response.json().get('data', {}).get('records', [])
+        
+        formatted_exams = [
+            {exam['name']: {'id': exam['id'], 'statTime': exam['statTime'], 'excelUrl': exam['excelUrl']}}
+            for exam in exams
+        ]
+        return formatted_exams
+    except requests.exceptions.HTTPError as err:
+        print ("HTTP Error:", err)
+    except requests.exceptions.RequestException as err:
+        print ("Oops: Something went wrong", err)
+
+
+def exam_details(exams, access_token):
+    # 定义请求头
+    headers = {
+        'SCHOOL-ID': '20125',
+        'Authorization': f'Bearer {access_token}'
+    }
+
+    # 用来存储最终结果的列表
+    results = []
+
+    # 对exams列表中的每一项进行处理
+    for exam in exams:
+        for exam_name, exam_info in exam.items():
+            # 准备请求参数
+            params = {
+                'id': exam_info['id'],
+                'studentId': 3124646,
+                'subjectId': 0
+            }
+            
+            # 发送GET请求
+            response = requests.get('https://wtjy.com/report/statstudentreport/getsrscoreoverview', headers=headers, params=params)
+
+            # 检查响应状态码
+            if response.status_code != 200:
+                print("请求失败，状态码：", response.status_code)
+                continue
+
+            # 解析响应的JSON数据
+            data = response.json().get('data')
+            
+            # 检查响应数据是否为空
+            if not data:
+                print("请求数据为空或格式不正确，请检查API响应")
+                continue
+            
+            # 初始化结构
+            exam_result = {exam_name: {
+                'id': exam_info['id'],
+                'excelUrl': exam_info['excelUrl'],
+                'totalscore': None,
+                'totalclassrank': None,
+                'totalschoolrank': None,
+                'scores': []
+            }}
+            
+            # 遍历返回的科目数据
+            for subject in data:
+                # 检查是否为总分
+                if subject['subjectId'] == 0:
+                    exam_result[exam_name]['totalscore'] = subject['score']
+                    exam_result[exam_name]['totalclassrank'] = subject['classRank']
+                    exam_result[exam_name]['totalschoolrank'] = subject['schoolRank']
+                else:
+                    # 如果answerSheetUrl不是null则分离URL
+                    answer_urls = subject['answerSheetUrl'].split('，') if subject['answerSheetUrl'] else ['', '']
+                    
+                    subject_results = {
+                        'subject': subject['subjectName'],
+                        'score': subject['score'],
+                        'classrank': subject['classRank'],
+                        'schoolrank': subject['schoolRank'],
+                        'answerpaperA': answer_urls[0],
+                        'answerpaperB': answer_urls[1] if len(answer_urls) > 1 else ''
+                    }
+                    # 添加到科目得分列表
+                    exam_result[exam_name]['scores'].append(subject_results)
+            
+            # 添加到结果列表
+            results.append(exam_result)
+    
+    return results
+
+
+exams_info = fetch_exams_info('41ec7f75-f300-4e98-8f4e-b34cafb80cba')
+#print(exams_info)
+
+# 获取格式化数据
+formatted_results = exam_details(exams_info, '41ec7f75-f300-4e98-8f4e-b34cafb80cba')
+
+# 打印结果
+print(formatted_results)
+
---
+++ main.py
@@ -0,0 +1,93 @@
+import flet as ft
+import grass
+
+username = "luoxin988"
+password = "123456"
+
+access_token = grass.get_access_token(username, password)['access_token']
+examsinfo = grass.fetch_exams_info(access_token)
+exams = grass.exam_details(examsinfo, access_token)
+
+
+def main(page: ft.Page):
+    # 假设 data 是您从上文中获取的考试成绩数据
+    #exams = [{'高2021级周考（2）理科': {'id': 16063, 'excelUrl': 'https://static.wtjy.com/temp/export/16063/kxyT0omI/115a2b58c68844614eb6ad5cc34e102a.zip', 'totalscore': 546.5, 'totalclassrank': 4, 'totalschoolrank': 334, 'scores': [{'subject': '语文', 'score': 102.0, 'classrank': 27, 'schoolrank': 687, 'answerpaperA': 'https://static.wtjy.com/temp/scan/86725/12/PC11762_86725_0012_0003_A_adjust.jpg', 'answerpaperB': 'https://static.wtjy.com/temp/scan/86725/12/PC11762_86725_0012_0003_B_adjust.jpg'}, {'subject': '数学', 'score': 93.0, 'classrank': 37, 'schoolrank': 1011, 'answerpaperA': 'https://static.wtjy.com/temp/scan/86726/8/PC11762_86726_0008_0003_A_adjust.jpg', 'answerpaperB': 'https://static.wtjy.com/temp/scan/86726/8/PC11762_86726_0008_0003_B_adjust.jpg'}, {'subject': '英语', 'score': 129.5, 'classrank': 2, 'schoolrank': 76, 'answerpaperA': 'https://static.wtjy.com/temp/scan/86727/5/PC11762_86727_0005_0003_A_adjust.jpg', 'answerpaperB': 'https://static.wtjy.com/temp/scan/86727/5/PC11762_86727_0005_0003_B_adjust.jpg'}, {'subject': '物理', 'score': 87.0, 'classrank': 4, 'schoolrank': 194, 'answerpaperA': 'https://static.wtjy.com/temp/scan/86728/2/PC11862_86728_0002_0002_A_adjust.jpg', 'answerpaperB': 'https://static.wtjy.com/temp/scan/86728/2/PC11862_86728_0002_0002_B_adjust.jpg'}, {'subject': '化学', 'score': 74.0, 'classrank': 19, 'schoolrank': 581, 'answerpaperA': 'https://static.wtjy.com/temp/scan/86728/2/PC11862_86728_0002_0002_A_adjust.jpg', 'answerpaperB': 'https://static.wtjy.com/temp/scan/86728/2/PC11862_86728_0002_0002_B_adjust.jpg'}, {'subject': '生物', 'score': 61.0, 'classrank': 21, 'schoolrank': 581, 'answerpaperA': 'https://static.wtjy.com/temp/scan/86728/2/PC11862_86728_0002_0002_A_adjust.jpg', 'answerpaperB': 'https://static.wtjy.com/temp/scan/86728/2/PC11862_86728_0002_0002_B_adjust.jpg'}, {'subject': '理综合', 'score': 222.0, 'classrank': 4, 'schoolrank': 320, 'answerpaperA': 'https://static.wtjy.com/temp/scan/86728/2/PC11862_86728_0002_0002_A_adjust.jpg', 'answerpaperB': 'https://static.wtjy.com/temp/scan/86728/2/PC11862_86728_0002_0002_B_adjust.jpg'}, {'subject': '日语', 'score': 0.0, 'classrank': 0, 'schoolrank': 0, 'answerpaperA': '', 'answerpaperB': ''}]}}, {'高2021级周考（1）理科': {'id': 15691, 'excelUrl': 'https://static.wtjy.com/temp/export/15691/8fAiBGmx/956b25ec9c2f0ead5ccd5c5a531fb803.zip', 'totalscore': 504.5, 'totalclassrank': 11, 'totalschoolrank': 519, 'scores': [{'subject': '语文', 'score': 95.0, 'classrank': 31, 'schoolrank': 795, 'answerpaperA': 'https://static.wtjy.com/temp/scan/85867/2/PC11805_85867_0002_0038_A_adjust.jpg', 'answerpaperB': 'https://static.wtjy.com/temp/scan/85867/2/PC11805_85867_0002_0038_B_adjust.jpg'}, {'subject': '数学', 'score': 94.0, 'classrank': 23, 'schoolrank': 883, 'answerpaperA': 'https://static.wtjy.com/temp/scan/85868/4/PC11862_85868_0004_0059_A_adjust.jpg', 'answerpaperB': 'https://static.wtjy.com/temp/scan/85868/4/PC11862_85868_0004_0059_B_adjust.jpg'}, {'subject': '英语', 'score': 130.5, 'classrank': 3, 'schoolrank': 186, 'answerpaperA': 'https://static.wtjy.com/temp/scan/85869/5/PC11862_85869_0005_0007_A_adjust.jpg', 'answerpaperB': 'https://static.wtjy.com/temp/scan/85869/5/PC11862_85869_0005_0007_B_adjust.jpg'}, {'subject': '物理', 'score': 53.0, 'classrank': 19, 'schoolrank': 696, 'answerpaperA': 'https://static.wtjy.com/temp/scan/85870/4/PC11805_85870_0004_0007_A_adjust.jpg', 'answerpaperB': 'https://static.wtjy.com/temp/scan/85870/4/PC11805_85870_0004_0007_B_adjust.jpg'}, {'subject': '化学', 'score': 60.0, 'classrank': 37, 'schoolrank': 716, 'answerpaperA': 'https://static.wtjy.com/temp/scan/85870/4/PC11805_85870_0004_0007_A_adjust.jpg', 'answerpaperB': 'https://static.wtjy.com/temp/scan/85870/4/PC11805_85870_0004_0007_B_adjust.jpg'}, {'subject': '生物', 'score': 72.0, 'classrank': 12, 'schoolrank': 337, 'answerpaperA': 'https://static.wtjy.com/temp/scan/85870/4/PC11805_85870_0004_0007_A_adjust.jpg', 'answerpaperB': 'https://static.wtjy.com/temp/scan/85870/4/PC11805_85870_0004_0007_B_adjust.jpg'}, {'subject': '理综合', 'score': 185.0, 'classrank': 18, 'schoolrank': 578, 'answerpaperA': 'https://static.wtjy.com/temp/scan/85870/4/PC11805_85870_0004_0007_A_adjust.jpg', 'answerpaperB': 'https://static.wtjy.com/temp/scan/85870/4/PC11805_85870_0004_0007_B_adjust.jpg'}, {'subject': '日语', 'score': 0.0, 'classrank': 0, 'schoolrank': 0, 'answerpaperA': '', 'answerpaperB': ''}]}}]
+
+    global exams
+    
+    # 创建一个可滚动的视图，以适应可能的多个考试卡片
+    page.scroll = True
+    audio1 = ft.Audio(
+        src="/ScoreTrackin'Superstar2.mp3", autoplay=True
+        #src="/勇敢说不.mp3", autoplay=True
+    )
+    page.overlay.append(audio1)
+
+    page.appbar = ft.AppBar(
+        leading=ft.Icon(ft.icons.PALETTE),
+        leading_width=40,
+        title=ft.Text("大狗屎查成绩1.0"),
+        center_title=False,
+        bgcolor=ft.colors.SURFACE_VARIANT,
+        actions=[
+            ft.IconButton(ft.icons.MUSIC_OFF, on_click=lambda _: audio1.pause()),
+        ],
+    )
+    # 遍历数据以创建每个考试的卡片
+    for exam_data in exams:
+        for exam_name, details in exam_data.items():
+            score_cards = [
+                ft.Row(
+                    [
+                        ft.Text(f"{subject['subject']}:", size=16),
+                        ft.Text(f"得分: {subject['score']}"),
+                        ft.Text(f"班级排名: {subject['classrank']}"),
+                        ft.Text(f"年级排名: {subject['schoolrank']}"),
+                    ],
+                    alignment="spaceAround"
+                )
+                for subject in details["scores"]
+            ]
+
+            subject_buttons = []
+
+            # 为特定科目 “语文”, “数学”, “英语”, “理综” 创建答题卡按钮
+            for subject in details["scores"]:
+                if subject["subject"] in ["语文", "数学", "英语", "理综合"]:
+                    answer_sheet_A = subject.get("answerpaperA")
+                    answer_sheet_B = subject.get("answerpaperB")
+                    subject_button = ft.ElevatedButton(
+                        text=subject["subject"],
+                        on_click=lambda _, a=answer_sheet_A, b=answer_sheet_B: (
+                            page.launch_url(a), page.launch_url(b)
+                        ) if a and b else None  # 只有当两个链接都存在时才会打开
+                    )
+                    subject_buttons.append(subject_button)
+
+            # 创建查看Excel按钮
+            excel_button = ft.ElevatedButton(
+                text="查看Excel",
+                on_click=lambda _, url=details["excelUrl"]: page.launch_url(url),
+            )
+            
+            # 创建一个卡片来显示考试名称、成绩、排名和答题卡按钮
+            exam_card = ft.Card(
+                content=ft.Column(
+                    [
+                        ft.Text(exam_name, size=20, weight="bold"),
+                        ft.Text(f"总分: {details['totalscore']}"),
+                        ft.Text(f"班级排名: {details['totalclassrank']}"),
+                        ft.Text(f"年级排名: {details['totalschoolrank']}"),
+                        *score_cards,
+                        ft.Row(subject_buttons, wrap=True),
+                        excel_button
+                    ],
+                ),
+            )
+
+            page.add(exam_card)
+
+    # 全部内容添加后一次性更新页面
+    page.update()
+
+ft.app(target=main)
